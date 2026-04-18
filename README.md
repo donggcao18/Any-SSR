0f1d61ccfa486081456161732a06a8f39dc93f63
Khanh Tran <ankhanhtran02@gmail.com>
2026-04-19 00:12:14 +0700

    Adapt O-LoRA

--- README.md
+++ README.md
@@ -17,30 +17,12 @@ We recommend using the [Anaconda](https://anaconda.org/) to install the developm
 git clone --depth=1 https://github.com/ZHUANGHP/Any-SSR.git
 
 cd Any-SSR
-conda env create -f environment.yaml
+conda env create -n anyssr-olora
+conda activate anyssr-olora
+pip install -r requirements.txt
 
 ```
 ## Quick Start
-All the data after processing can be downloaded from [Trace Benchmark](https://drive.google.com/file/d/1S0SmU0WEw5okW_XvP2Ns0URflNzZq6sV/view)
-
-You should specify the directory to the dataset and the pretrained model (we used [Llama-2-7b-chat-hf](https://huggingface.co/meta-llama/Llama-2-7b-chat-hf)). You can download the pre-trained weight via the code or directly download it from huggingface. 
-
-After finishing dataset and pre-trained weight downloading, use
-```bash
-python train_router_ana_continual.py
-```
-to train the router weight recursively, then use
-```bash
-
-python eval_router_ana.py
-```
-to generate routing accuracy.
-
-<div align="center">
-<img src="imgs/router_eval.png" width="800px">
-</div>
-
-The router can have nearly 100 percent accuracy in the experiments.
 
 ### Lora Model Training
 
@@ -50,11 +32,6 @@ bash train_lora.sh
 ```
 to train a lora model for each task in the Trace dataset. 
 
-### Evaluate
-```bash
-bash scripts/inference.sh
-```
-This commend will start the inference. Before inference, please specify the directories to the lora models, router weights and other in the script. 
 
 <div align="center">
 <img src="imgs/infer_results.png" width="800px">
--- training/params.py
+++ training/params.py
@@ -1,29 +1,38 @@
-from model.Dynamic_network.PP import PP
-from model.Dynamic_network.L2P import L2P
-from model.Regular.LwF import LwF
-from model.Regular.EWC import EWC
-from model.Regular.GEM import GEM
-from model.Regular.OGD import OGD
-from model.Replay.MbPAplusplus import MbPAplusplus
-from model.Replay.LFPT5 import LFPT5
+# from model.Dynamic_network.PP import PP
+# from model.Dynamic_network.L2P import L2P
+# from model.Regular.LwF import LwF
+# from model.Regular.EWC import EWC
+# from model.Regular.GEM import GEM
+# from model.Regular.OGD import OGD
+# from model.Replay.MbPAplusplus import MbPAplusplus
+# from model.Replay.LFPT5 import LFPT5
 from model.Regular.O_LoRA import O_LoRA
 from model.base_model import CL_Base_Model
-from model.lora import lora
+# from model.lora import lora
 
 
 
-Method2Class = {"PP":PP,
-                "EWC":EWC,
-                "GEM":GEM,
-                "OGD":OGD,
-                "LwF":LwF,
-                "L2P":L2P,
-                "MbPA++":MbPAplusplus,
-                "LFPT5":LFPT5, 
+Method2Class = {
+                # "PP":PP,
+                # "EWC":EWC,
+                # "GEM":GEM,
+                # "OGD":OGD,
+                # "LwF":LwF,
+                # "L2P":L2P,
+                # "MbPA++":MbPAplusplus,
+                # "LFPT5":LFPT5, 
                 "O-LoRA":O_LoRA,
                 "base":CL_Base_Model,
-                "lora":lora,
-                "anamoe":lora}
+                # "lora":lora,
+                # "anamoe":lora
+                }
 
-AllDatasetName = ["C-STANCE","FOMC","MeetingBank","Papyrus-f","Py150","ScienceQA","ToolBench","NumGLUE-cm","NumGLUE-ds","20Minuten"]
+AllDatasetName = ["CONCODE",
+                  "CodeTrans",
+                  "CodeSearchNet",
+                  "BFP",
+                  "KodCode",
+                  "RunBugRun",
+                  "TheVault_Csharp",
+                  "CoST"]
 
