8d125d35e39a2534fb82a9fc640602c1230a0727
donggcao18 <caovdongg18@gmail.com>
2026-05-19 13:58:28 +0700

    +

--- scripts/code_t5/train_o_lora.sh
+++ scripts/code_t5/train_o_lora.sh
@@ -2,7 +2,13 @@
 export HF_HOME=./.cache
 export HF_DATASETS_CACHE=./.cache
 export CUDA_VISIBLE_DEVICES=0
+export SCRATCH_ROOT=/data/scratch/projects/punim1928/east/CodeGR/Dense/any-ssr/.cache
+mkdir -p "$SCRATCH_ROOT/torch_extensions" "$SCRATCH_ROOT/tmp"
 
+export TORCH_EXTENSIONS_DIR=$SCRATCH_ROOT/torch_extensions
+export TMPDIR=$SCRATCH_ROOT/tmp
+export TEMP=$TMPDIR
+export TMP=$TMPDIR
 set -euo pipefail
 
 port=$(shuf -i25000-30000 -n1)
@@ -10,9 +16,9 @@ port=$(shuf -i25000-30000 -n1)
 deepspeed --master_port "$port" training/main_anamoe.py \
    --model_name_or_path Salesforce/codet5p-770m \
    --data_path "" \
-   --per_device_train_batch_size 16 \
-   --per_device_eval_batch_size 8 \
-   --gradient_accumulation_steps 2 \
+   --per_device_train_batch_size 32 \
+   --per_device_eval_batch_size 32 \
+   --gradient_accumulation_steps 1 \
    --learning_rate 1e-4 \
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
