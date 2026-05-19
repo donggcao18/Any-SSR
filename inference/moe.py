14d507079bf4cf5651cf2e0c5200fcc2505244fd
tranquyenbk173 <ankhanhtran02@gmail.com>
2026-05-19 08:28:36 -0400

    +

--- inference/moe.py
+++ inference/moe.py
@@ -243,12 +243,7 @@ class NewQwen2ForCausalLM(Qwen2ForCausalLM):
         )
 
         hidden_states = outputs[0]
-        if self.config.pretraining_tp > 1:
-            lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
-            logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
-            logits = torch.cat(logits, dim=-1)
-        else:
-            logits = self.lm_head(hidden_states)
+        logits = self.lm_head(hidden_states)
         logits = logits.float()
 
         loss = None
